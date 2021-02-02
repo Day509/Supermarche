e0ead677b100194f626515cdf16e0428fd9426e7
Davidson Charlot <davidson.charlot@gmail.com>
2021-02-02 12:51:44 +0100

    Affichage de l'arriere plan

--- supermarche.py
+++ supermarche.py
@@ -3,12 +3,16 @@ import pygame
 pygame.init()
 
 pygame.display.set_caption('Le jeu')
-pygame.display.set_mode((500, 300))
+fenetre = pygame.display.set_mode((1920, 1080))
+arriere_plan = pygame.image.load('Fenetre/supermarche/mycollection/png/grocery-store-2619380_1920.jpg')
+Ecran = fenetre.blit(arriere_plan, (0, 0))
+
 
 boucle = True
 
 while boucle:
 
+    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             boucle = False
