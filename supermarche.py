11e3008e1c447fa84e1c9490ee7289f7029bb949
Davidson Charlot <davidson.charlot@gmail.com>
2021-02-02 17:55:01 +0100

    Ajout de bouton. Tentative #1

--- supermarche.py
+++ supermarche.py
@@ -4,10 +4,10 @@ pygame.init()
 
 pygame.display.set_caption('Supermarché')
 fenetre = pygame.display.set_mode((1920, 1080))
-arriere_plan = pygame.image.load('Fenetre/supermarche/mycollection/png/grocery-store-2619380_1920.jpg')
+arriere_plan = pygame.image.load('Fenetre/supermarche/mycollection/png/fond_supermarché.jpg')
 Ecran = fenetre.blit(arriere_plan, (0, 0))
 
-rectangle = pygame.Rect((1920 * (2 / 5)), 40, 400, 100)
+rectangle = pygame.Rect((1920 * (2 / 5)), 840, 400, 100)
 pygame.draw.rect(fenetre, (0, 0, 0), rectangle)
 
 boucle = True
@@ -17,5 +17,9 @@ while boucle:
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            boucle = False
+            pygame.quit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
             boucle = False
             pygame.quit()
