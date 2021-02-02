0b44eb37ed1efd3012ce52798d382b8d2ce2b780
Davidson Charlot <davidson.charlot@gmail.com>
2021-02-02 14:25:51 +0100

    Affiche un rectangle (emplacement potentielle d'un titre)

--- supermarche.py
+++ supermarche.py
@@ -2,11 +2,13 @@ import pygame
 
 pygame.init()
 
-pygame.display.set_caption('Le jeu')
+pygame.display.set_caption('Supermarché')
 fenetre = pygame.display.set_mode((1920, 1080))
 arriere_plan = pygame.image.load('Fenetre/supermarche/mycollection/png/grocery-store-2619380_1920.jpg')
 Ecran = fenetre.blit(arriere_plan, (0, 0))
 
+rectangle = pygame.Rect((1920 * (2 / 5)), 40, 400, 100)
+pygame.draw.rect(fenetre, (0, 0, 0), rectangle)
 
 boucle = True
 
