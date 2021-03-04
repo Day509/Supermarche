78fc524c7b076ba316248cf8e7d02b460ed2425d
Davidson Charlot <davidson.charlot@gmail.com>
2021-03-04 13:27:49 +0100

    Finalisation du panier et de la validation de l'achat

--- supermarche.py
+++ supermarche.py
@@ -9,10 +9,17 @@ hauteur = 1080
 fenetre = pygame.display.set_mode((largeur, hauteur))
 arriere_plan = pygame.image.load('Fenetre/supermarche/mycollection/png/fond_supermarché.jpg')
 arriere_plan_2 = pygame.image.load('Fenetre/supermarche/mycollection/png/fond_supermarche_flou.png')
+arriere_plan_3 = pygame.image.load('Fenetre/supermarche/mycollection/png/fond_supermarche_flou.png')
+
 bouton_commencer = pygame.image.load('Fenetre/supermarche/mycollection/png/Bouton_Commencer.jpg')
 bouton_commencer = pygame.transform.scale(bouton_commencer, (300, 100))
 bouton_commencer_rect = bouton_commencer.get_rect()
 bouton_commencer_rect.center = (largeur / 2, (hauteur - hauteur / 6) - 10)
+
+bouton_valider_panier = pygame.image.load('Fenetre/supermarche/mycollection/png/bouton_valider_panier.png')
+bouton_valider_panier = pygame.transform.scale(bouton_valider_panier, (120, 50))
+bouton_valider_panier_rect = bouton_valider_panier.get_rect()
+bouton_valider_panier_rect.topright = (largeur - 23, 305)
 
 charcuterie = pygame.image.load('Fenetre/supermarche/mycollection/png/001-salami.png')
 charcuterie_rect = charcuterie.get_rect()
@@ -45,12 +52,17 @@ poisson_rect.center = (largeur - largeur / 4, hauteur - hauteur / 3)
 panier = [0, 0, 0, 0, 0, 0, 0]
 print(panier)
 
+prix_article = [4.0, 1.0, 1.5, 3.5, 3.7, 2.3, 2.1]
+prix_totale = 0
+
 achat_terminer = pygame.draw.rect(arriere_plan_2, (100, 193, 70), pygame.Rect(largeur - 170, 0, 170, 300), 5)
 achat_terminer.topright = (largeur, 0)
 
-police = pygame.font.SysFont('Arial', 40)
-police.set_italic(True)
+police = pygame.font.SysFont('Arial', 40, False, True)
 police.set_underline(True)
+
+police_panier_fini = pygame.font.SysFont("Rockwell", 50)
+police_panier_fini.set_bold(True)
 
 titre_panier = police.render("Panier:", True, (0, 0, 0))
 
@@ -87,6 +99,7 @@ while boucle:
                 fenetre.blit(fruit, fruit_rect)
                 fenetre.blit(poisson, poisson_rect)
                 fenetre.blit(titre_panier, (1777, 3))
+                fenetre.blit(bouton_valider_panier, bouton_valider_panier_rect)
                 charcuterie_panier_police = police_article.render("Charcuterie: " + str(panier[0]), True,
                                                                   (255, 255, 255))
                 fenetre.blit(charcuterie_panier_police, (1757, coord_y_article))
@@ -103,7 +116,17 @@ while boucle:
                 poisson_panier_police = police_article.render("poisson: " + str(panier[6]), True, (255, 255, 255))
                 fenetre.blit(poisson_panier_police, (1757, coord_y_article + 180))
 
-                if charcuterie_rect.collidepoint(event.pos):
+                if bouton_valider_panier_rect.collidepoint(event.pos):
+                    Ecran = fenetre.blit(arriere_plan_3, (0, 0))
+                    for i in panier:
+                        prix_totale += prix_article[i] * panier[i]
+
+                    panier_fini = police_panier_fini.render(
+                        "Votre panier final est fini, Vous devez réglé: " + str(prix_totale) + "€", True, (0, 0, 0))
+
+                    fenetre.blit(panier_fini, ((largeur/3)-80, (hauteur/2)-200))
+
+                elif charcuterie_rect.collidepoint(event.pos):
                     panier[0] += 1
 
                 elif fleur_rect.collidepoint(event.pos):
@@ -123,5 +146,3 @@ while boucle:
 
                 elif poisson_rect.collidepoint(event.pos):
                     panier[6] += 1
-
-print(panier)
